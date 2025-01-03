43fa189bf660e026acb8f8e51238b7e2e99899f8
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-03 09:37:01 +0600

    Add home route to GoRouter for navigation to HomeScreen

--- lib/app/trustbox_router.dart
+++ lib/app/trustbox_router.dart
@@ -1,14 +1,18 @@
 import 'package:go_router/go_router.dart';
+import 'package:trustbox/views/screens/home.dart';
 import 'package:trustbox/views/screens/splash.dart';
 
 GoRouter getRouter = GoRouter(
-    initialLocation: '/', // Initial route when the app starts
-    routes: [
-      // Define the login screen route
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const SplashScreen(),
-      ),
-      
-    ],
-  );
+  initialLocation: '/', // Initial route when the app starts
+  routes: [
+    // Define the login screen route
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
