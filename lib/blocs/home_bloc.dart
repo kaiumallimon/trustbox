d42fb45de8e32bb6c2233d8734b176db9a8d96bb
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-03 10:18:40 +0600

    Add HomeBloc with error handling and user data retrieval

--- lib/blocs/home_bloc.dart
+++ lib/blocs/home_bloc.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trustbox/repositories/auth_repository.dart';
 
 @immutable
 abstract class HomeEvent {}
 
 class GetHomeData extends HomeEvent {}
-
-
 
 @immutable
 abstract class HomeState {}
@@ -15,7 +16,34 @@ class HomeInitial extends HomeState {}
 class HomeLoading extends HomeState {}
 
 class HomeLoaded extends HomeState {
-  final String data;
+  final String? image;
 
-  HomeLoaded(this.data);
+  HomeLoaded(this.image);
+}
+
+class HomeError extends HomeState {
+  final String message;
+
+  HomeError(this.message);
+}
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial()) {
+    on<GetHomeData>((event, emit) async {
+      emit(HomeLoading());
+      try {
+        await Future.delayed(Duration(seconds: 2));
+
+        User? user = AuthRepository().getCurrentUser();
+
+        if (user != null) {
+          emit(HomeLoaded(user.photoURL));
+        } else {
+          emit(HomeError('User not found'));
+        }
+      } catch (e) {
+        emit(HomeError(e.toString()));
+      }
+    });
+  }
 }
