7bcd556c783e3b8b87f1e50d990580252c4024ba
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-03 10:19:06 +0600

    Add GeneratePassScreen widget for password generation functionality

---
+++ lib/views/screens/generate_pass_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GeneratePassScreen extends StatelessWidget {
+  const GeneratePassScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Generate Pass Screen'),
+    );
+  }
+}
