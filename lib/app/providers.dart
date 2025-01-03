a8ed1a5607d59726c1eab8f1d416aae28624c72a
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-03 08:44:39 +0600

    Add AuthCubit provider to manage authentication state

--- lib/app/providers.dart
+++ lib/app/providers.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../blocs/auth_bloc.dart';
 import '../blocs/splash_bloc.dart';
+import '../repositories/auth_repository.dart';
 
 final providers = [
   // splash bloc
   BlocProvider<SplashBloc>(
     create: (context) => SplashBloc(),
+  ),
+
+  // auth bloc
+  BlocProvider<AuthCubit>(
+    create: (context) => AuthCubit(AuthRepository()),
   ),
 ];
