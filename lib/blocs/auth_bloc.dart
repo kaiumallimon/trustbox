e15064219a7ee5be849200eab53f818758c8d457
Kaium Al Limon <limon.3cs@gmail.com>
2025-01-03 09:36:51 +0600

    Refactor AuthCubit to AuthBloc and implement event handling for user authentication

--- lib/blocs/auth_bloc.dart
+++ lib/blocs/auth_bloc.dart
@@ -2,10 +2,72 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trustbox/repositories/auth_repository.dart';
+import 'package:trustbox/models/auth_response_model.dart';
 
-import '../models/auth_response_model.dart';
+// AuthBloc Class
 
-// auth state
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository authRepository;
+
+  AuthBloc(this.authRepository) : super(AuthInitial()) {
+    // Handle events and emit states
+    on<CheckUserStatus>(_onCheckUserStatus);
+    on<SignInWithGoogle>(_onSignInWithGoogle);
+    on<SignOut>(_onSignOut);
+  }
+
+  // Check if the user is already signed in
+  Future<void> _onCheckUserStatus(
+      CheckUserStatus event, Emitter<AuthState> emit) async {
+    try {
+      final User? user = authRepository.getCurrentUser();
+      if (user != null) {
+        emit(AuthSignedIn(user));
+      } else {
+        emit(AuthSignedOut());
+      }
+    } catch (e) {
+      emit(AuthError("Error checking user status: $e"));
+    }
+  }
+
+  // Sign in using Google
+  Future<void> _onSignInWithGoogle(
+      SignInWithGoogle event, Emitter<AuthState> emit) async {
+    emit(AuthLoading()); // Show loading state while signing in
+    try {
+      final AuthResponseModel response =
+          await authRepository.signInWithGoogle();
+
+      if (response.user != null) {
+        emit(AuthSignedIn(response.user!));
+      } else {
+        emit(AuthError(response.message));
+      }
+    } catch (e) {
+      emit(AuthError("Error signing in: $e"));
+    }
+  }
+
+  // Sign out the user
+  Future<void> _onSignOut(SignOut event, Emitter<AuthState> emit) async {
+    try {
+      await authRepository.signOut();
+      emit(AuthSignedOut());
+    } catch (e) {
+      emit(AuthError("Error signing out: $e"));
+    }
+  }
+}
+
+@immutable
+abstract class AuthEvent {}
+
+class CheckUserStatus extends AuthEvent {}
+
+class SignInWithGoogle extends AuthEvent {}
+
+class SignOut extends AuthEvent {}
 
 @immutable
 abstract class AuthState {}
@@ -24,52 +86,4 @@ class AuthSignedOut extends AuthState {}
 class AuthError extends AuthState {
   final String message;
   AuthError(this.message);
-}
-
-// auth cubit
-
-class AuthCubit extends Cubit<AuthState> {
-  final AuthRepository authRepository;
-
-  AuthCubit(this.authRepository) : super(AuthInitial());
-
-  // Check if the user is already signed in
-  Future<void> checkUserStatus() async {
-    try {
-      final User? user = authRepository.getCurrentUser();
-      if (user != null) {
-        print('User is signed in');
-        emit(AuthSignedIn(user));
-      } else {
-        print('User is signed out');
-        emit(AuthSignedOut());
-      }
-    } catch (e) {
-      emit(AuthError("Error checking user status: $e"));
-    }
-  }
-
-  // Sign in using Google
-  Future<void> signInWithGoogle() async {
-    emit(AuthLoading()); // Show loading state while signing in
-    final AuthResponseModel response = await authRepository.signInWithGoogle();
-
-    if (response.user != null) {
-      print('User signed in: ${response.user!.displayName}');
-      emit(AuthSignedIn(response.user!));
-    } else {
-      print('Error signing in: ${response.message}');
-      emit(AuthError(response.message));
-    }
-  }
-
-  // Sign out the user
-  Future<void> signOut() async {
-    try {
-      await authRepository.signOut();
-      emit(AuthSignedOut());
-    } catch (e) {
-      emit(AuthError("Error signing out: $e"));
-    }
-  }
 }
